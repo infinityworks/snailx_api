124adf5828fe9c677dd5c9c6a2e1a80c8dd17413
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-04 18:02:04 +0100

    Return json containing auth token from auth route.

--- api/auth/auth.py
+++ api/auth/auth.py
@@ -6,6 +6,7 @@ from flask import current_app as app
 auth_blueprint = Blueprint('auth', __name__)
 HARDCODED_USER_ID = 1
 
+
 @auth_blueprint.route('/auth/token')
 def auth_token():
     """
@@ -18,10 +19,29 @@ def auth_token():
             'iat': datetime.datetime.utcnow(),
             'sub': HARDCODED_USER_ID
         }
-        return jwt.encode(
+
+        token = jwt.encode(
             payload,
             app.config.get('SECRET_KEY'),
             algorithm='HS256'
         )
+
+        return {"token": token.decode('utf-8')}
     except Exception as e:
         return e
+
+
+@staticmethod
+def decode_auth_token(auth_token):
+    """
+    Decodes the auth token
+    :param auth_token:
+    :return: integer|string
+    """
+    try:
+        payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
--- api/globals/globals.py
+++ api/globals/globals.py
@@ -3,9 +3,11 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config.config import Config
 from auth.auth import auth_blueprint
+import os
 
 app = FlaskAPI(__name__)
 app.config.from_object(Config)
+SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
