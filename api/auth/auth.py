6fafa9c8d843dbdc8b816eaf45deb668bb94d1e4
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-04 17:43:54 +0100

    Add route for retrieving auth token.

--- api/auth/auth.py
+++ api/auth/auth.py
@@ -1,4 +1,27 @@
 from flask import Blueprint, request, make_response, jsonify
-from flask.views import MethodView
+import datetime
+import jwt
+from flask import current_app as app
 
 auth_blueprint = Blueprint('auth', __name__)
+HARDCODED_USER_ID = 1
+
+@auth_blueprint.route('/auth/token')
+def auth_token():
+    """
+        Generates the Auth Token
+    :return: string
+    """
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=30),
+            'iat': datetime.datetime.utcnow(),
+            'sub': HARDCODED_USER_ID
+        }
+        return jwt.encode(
+            payload,
+            app.config.get('SECRET_KEY'),
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return e
--- api/globals/globals.py
+++ api/globals/globals.py
@@ -9,5 +9,4 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-#app components
 app.register_blueprint(auth_blueprint)
