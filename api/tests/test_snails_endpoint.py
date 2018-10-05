5035be58038b63f082456e8b851341af031b2bfe
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-05 13:07:04 +0100

    Add passing tests for snails endpoint with auth.

--- api/tests/test_snails_endpoint.py
+++ api/tests/test_snails_endpoint.py
@@ -1,13 +1,40 @@
 import unittest
+from globals.globals import app
 
 
 class TestSnailsEndpoint(unittest.TestCase):
 
+    def setUp(self):
+        self.app = app.test_client()
+
     def test_get_snails_without_auth(self):
-        self.fail()
+        with self.app as client:
+            response = client.get("/snails").get_json()
+            expected_result = {
+                'status': 'Failed',
+                'message': 'Unauthorized'
+            }
+            self.assertEqual(response, expected_result)
 
     def test_get_snails_with_auth(self):
-        self.fail()
+        token = ""
+        with self.app as client:
+            response = client.get("/auth/token").get_json()
+            token = response['token']
+
+        headers = {'Authorization': token}
+        with self.app as client:
+            response = client.get("/snails", headers=headers).get_json()
+            expected_result = {
+                "id": 1,
+                "name": "Terry",
+                "age": 13,
+                "trainer": {
+                        "id": 17,
+                        "name": "gazza"
+                }
+            }
+            self.assertEqual(response, expected_result)
 
 
 if __name__ == '__main__':
