680a736537a72e994a675d69fae2136dbea7203e
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-05 12:57:06 +0100

    Add passing test for auth endpoint.

--- api/tests/test_auth_endpoint.py
+++ api/tests/test_auth_endpoint.py
@@ -1,9 +1,21 @@
 import unittest
+from globals.globals import app
+from tests.test_auth import MockAuth
+from flask import jsonify
+from auth.auth import encode_auth_token
+
 
 class TestAuthEndpoint(unittest.TestCase):
 
+    def setUp(self):
+        self.app = app.test_client()
+
     def test_get_auth_token(self):
-        self.fail()
+        with self.app as client:
+            response = client.get("/auth/token").get_json()
+
+            expectedResponse = {'token': response['token']}
+            self.assertEqual(response, expectedResponse)
 
 
 if __name__ == '__main__':
