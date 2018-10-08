046ebff4ce677d159f9b7b69ba2e52e09339e678
Michael Silverstone <michael.silverstone@infinityworks.com>
2018-10-08 14:17:13 +0100

    tinkering with mike ;)

--- api/db/models.py
+++ api/db/models.py
@@ -7,6 +7,7 @@ class Trainer(db.Model):
 
     def __repr__(self):
         return "<id: {}>".format(self.id)
+
     def get_trainer(self, id):
         trainer = self.query.filter_by(id=id).first()
         return trainer
@@ -18,55 +19,63 @@ class Snail(db.Model):
     trainer_id = db.Column(db.Integer, db.ForeignKey('trainer.id'))
 
     def __repr__(self):
-       return "<id: {}>".format(self.id) 
+        return "<id: {}>".format(self.id)
+
     def get_snail(self, id):
         snail = self.query.filter_by(id=id).first()
         return snail  
 
+
 class RaceParticipants(db.Model):
-    id = db.Column(db.Integer(), unique=True, nullable=False, primary_key=True, autoincrement=True)
-    id_snail = db.Column(db.Integer(), db.ForeignKey("snail.id"))
+    id = db.Column(db.Integer(), unique=True, primary_key=True, autoincrement=True)
+    id_snail = db.Column(db.Integer(), db.ForeignKey("snail.id"), nullable=False)
     id_race = db.Column(db.Integer(), db.ForeignKey("race.id"))
     
     def __repr__(self):
         return "<id: {}>".format(self.id)
+
     def get_race_participants(self, id):
-        # id_race produces an error "unidentified variable id_race"
         race_participants = self.query.filter_by(id=id).first()
         return race_participants
 
+
 class Race(db.Model):
-    id = db.Column(db.Integer(), unique=True, nullable=False, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer(), unique=True, primary_key=True, autoincrement=True)
     date = db.Column(db.DateTime(), nullable=False)
     status = db.Column(db.String(), nullable=False)
     id_round = db.Column(db.Integer(), db.ForeignKey("round.id"))
 
     def __repr__(self):
         return "<id: {}>".format(self.id)
+
     def get_race(self, id):
         race = self.query.filter_by(id=id).first()
         return race
 
+
 class Round(db.Model):
-    id = db.Column(db.Integer(), unique=True, nullable=False, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer(), unique=True, primary_key=True, autoincrement=True)
     name = db.Column(db.String(12), nullable=False)
     start_date = db.Column(db.DateTime(), nullable=False)
 
     def __repr__(self):
         return "<id: {}>".format(self.id)
+
     def get_round(self, id):
         round = self.query.filter_by(id=id).first()
         return round
 
+
 class RaceResult(db.Model):
-    id = db.Column(db.Integer(), unique=True, nullable=False, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer(), unique=True, primary_key=True, autoincrement=True)
     position = db.Column(db.Integer(), nullable=False)
     time_to_finish = db.Column(db.DateTime(), nullable=False)
     did_not_finish = db.Column(db.String(3))
-    id_race_participants = db.Column(db.Integer(), db.ForeignKey("raceparticipants.id"))
+    id_race_participants = db.Column(db.Integer(), db.ForeignKey("race_participants.id"))
 
     def __repr__(self):
         return "<id: {}>".format(self.id)
+
     def get_race_result(self, id):
         race_result = self.query.filter_by(id=id).first()
         return race_result
