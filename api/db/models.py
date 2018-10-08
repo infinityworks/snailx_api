e5f30ef237711802a663324424bb2d447ee51dc3
Sandeep Sharda <sandeep.sharda@infinityworks.com>
2018-10-08 12:03:21 +0100

    added all models, snails, trainers, raceparticipants, raceresult, round

--- api/db/models.py
+++ api/db/models.py
@@ -4,7 +4,6 @@ from globals.globals import db
 class Trainer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(12), nullable=False)
-    #snail = db.relationship('Snail', backref='trainer', lazy=True)
 
     def __repr__(self):
         return "<id: {}>".format(self.id)
@@ -17,7 +16,6 @@ class Snail(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(12), nullable=True)
     trainer_id = db.Column(db.Integer, db.ForeignKey('trainer.id'))
-    #trainer_name = db.relationship("Trainer", foreign_keys=[trainer_id])
 
     def __repr__(self):
        return "<id: {}>".format(self.id) 
@@ -25,47 +23,50 @@ class Snail(db.Model):
         snail = self.query.filter_by(id=id).first()
         return snail  
 
-"""class RaceParticipants(db.Model):
+class RaceParticipants(db.Model):
     id = db.Column(db.Integer(), unique=True, nullable=False, primary_key=True, autoincrement=True)
-    id_Snail = db.Column(db.Integer(), nullable=False, ForeignKey("snail.id"))
-
+    id_snail = db.Column(db.Integer(), db.ForeignKey("snail.id"))
+    id_race = db.Column(db.Integer(), db.ForeignKey("race.id"))
+    
     def __repr__(self):
-        return "<Title: {}>".format(self.title)
+        return "<id: {}>".format(self.id)
+    def get_race_participants(self, id):
+        race_participants = self.query.filter_by(id=id_race).first()
+        return race_participants
 
 class Race(db.Model):
     id = db.Column(db.Integer(), unique=True, nullable=False, primary_key=True, autoincrement=True)
-    Date = db.Column(db.DateTime(), nullable=False)
-    Status = db.Column(db.String(), nullable=False)
-    id_Round = db.Column(db.Integer(), nullable=False)
-    id_RaceParticipants = db.Column(db.Integer(), nullable=False, ForeignKey("RaceParticipants.id"))
+    date = db.Column(db.DateTime(), nullable=False)
+    status = db.Column(db.String(), nullable=False)
+    id_round = db.Column(db.Integer(), db.ForeignKey("round.id"))
 
     def __repr__(self):
-        return "<Title: {}>".format(self.title)
+        return "<id: {}>".format(self.id)
+    def get_race(self, id):
+        race = self.query.filter_by(id=id).first()
+        return race
 
 class Round(db.Model):
     id = db.Column(db.Integer(), unique=True, nullable=False, primary_key=True, autoincrement=True)
-    Name = db.Column(db.String(12), nullable=False)
-    NumRaces = db.Column(db.Integer(), nullable=False)
-    StartDate = db.Column(db.DateTime(), nullable=False)
-    EndDate = db.Column(db.DateTime(), nullable=False)
+    name = db.Column(db.String(12), nullable=False)
+    start_date = db.Column(db.DateTime(), nullable=False)
 
     def __repr__(self):
-        return "<Title: {}>".format(self.title)
+        return "<id: {}>".format(self.id)
+    def get_round(self, id):
+        round = self.query.filter_by(id=id).first()
+        return round
 
 class RaceResult(db.Model):
     id = db.Column(db.Integer(), unique=True, nullable=False, primary_key=True, autoincrement=True)
-    id_Race = db.Column(db.Integer(), nullable=False, ForeignKey("Race.id"))
-    id_FirstPace = db.Column(db.Integer(), nullable=False, ForeignKey("Snail.id"))
-    id_SecondPlace = db.Column(db.Integer(), nullable=False, ForeignKey("Snail.id"))
-    id_ThirdPlace = db.Column(db.Integer(), nullable=False, ForeignKey("Snail.id"))
+    position = db.Column(db.Integer(), nullable=False)
+    time_to_finish = db.Column(db.DateTime(), nullable=False)
+    did_not_finish = db.Column(db.String(3))
+    id_race_participants = db.Column(db.Integer(), db.ForeignKey("raceparticipants.id"))
 
     def __repr__(self):
-        return "<Title: {}>".format(self.title)
+        return "<id: {}>".format(self.id)
+    def get_race_result(self, id):
+        race_result = self.query.filter_by(id=id).first()
 
-class TrainersSnails(db.Model):
-    id = db.Column(db.Integer(), unique=True, nullable=False, primary_key=True, autoincrement=True)
-    id_Snail = db.Column(db.Integer(), nullable=False, ForeignKey("Snail.id")) 
-
-    def __repr__(self):
-        return "<Title: {}>".format(self.title)"""
 
