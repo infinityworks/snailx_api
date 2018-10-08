65514ca0114b63458c17281cc68aaed05e733240
Malik Glossop <malik.glossop@infinityworks.com>
2018-10-08 15:34:21 +0100

    Added changes to models.py: removed duplicate return statement on 'Race' class, simplfiied return statements on 'get' methods, removed unneccesary unqiue=true.

--- api/db/models.py
+++ api/db/models.py
@@ -11,8 +11,7 @@ class Trainer(db.Model):
         return "<Trainer\nid: {}\n name: {}>".format(self.id, self.name)
 
     def get_trainer(self, id):
-        trainer = self.query.filter_by(id=id).first()
-        return trainer
+        return self.query.filter_by(id=id).first()
 
 
 class Snail(db.Model):
@@ -24,12 +23,11 @@ class Snail(db.Model):
         return "<Snail\nid: {}\n name: {}\n trainer_id: {}>".format(self.id, self.name, self.trainer_id)
 
     def get_snail(self, id):
-        snail = self.query.filter_by(id=id).first()
-        return snail  
+        return self.query.filter_by(id=id).first()
 
 
 class RaceParticipants(db.Model):
-    id = db.Column(db.Integer(), unique=True, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     id_snail = db.Column(db.Integer(), db.ForeignKey("snail.id"), nullable=False)
     id_race = db.Column(db.Integer(), db.ForeignKey("race.id"), nullable=False)
     
@@ -37,27 +35,24 @@ class RaceParticipants(db.Model):
         return "<Race Participants\nid: {}\n snail_id: {}\n race_id: {}>".format(self.id, self.snail_id, self.race_id)
 
     def get_race_participants(self, id):
-        race_participants = self.query.filter_by(id=id).first()
-        return race_participants
+        return self.query.filter_by(id=id).first()
 
 
 class Race(db.Model):
-    id = db.Column(db.Integer(), unique=True, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     date = db.Column(db.DateTime(), nullable=False)
     status = db.Column(db.String(), nullable=False)
     id_round = db.Column(db.Integer(), db.ForeignKey("round.id"), nullable=False)
 
     def __repr__(self):
         return "<Race\nid: {}\n date: {}\n status: {}\n round_id: {}>".format(self.id, self.date, self.status, self.round_id)
-        return "<id: {}>".format(self.id)
 
     def get_race(self, id):
-        race = self.query.filter_by(id=id).first()
-        return race
+        return self.query.filter_by(id=id).first()
 
 
 class Round(db.Model):
-    id = db.Column(db.Integer(), unique=True, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     name = db.Column(db.String(12), nullable=False)
     start_date = db.Column(db.DateTime(), nullable=False)
 
@@ -66,12 +61,11 @@ class Round(db.Model):
 
 
     def get_round(self, id):
-        round = self.query.filter_by(id=id).first()
-        return round
+        return self.query.filter_by(id=id).first()
 
 
 class RaceResult(db.Model):
-    id = db.Column(db.Integer(), unique=True, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     position = db.Column(db.Integer(), nullable=False)
     time_to_finish = db.Column(db.DateTime(), nullable=False)
     did_not_finish = db.Column(db.String(3))
@@ -81,5 +75,4 @@ class RaceResult(db.Model):
         return "<Race Result\nid: {}\n position: {}\n time_to_finish: {}\n did_not_finish: {}\n race_participants_id: {}>".format(self.id, self.position, self.time_to_finish, self.did_not_finish, self.race_participants_id)
 
     def get_race_result(self, id):
-        race_result = self.query.filter_by(id=id).first()
-        return race_result
+        return self.query.filter_by(id=id).first()
