9c46769ffbddfbd442b7204459ad72b8e6d3cbf4
Michael Silverstone <michael.silverstone@infinityworks.com>
2018-10-09 09:46:19 +0100

    fixing merge

--- api/main.py
+++ api/main.py
@@ -3,7 +3,6 @@ sys.path.insert(0, '/vagrant/repos/snailx_api/api')
 from globals.globals import app
 from db.models import Snail, Trainer
 from auth.auth import authenticate_request, unauthorised_response
-from flask import request, make_response
 
 
 @app.route('/snails')
