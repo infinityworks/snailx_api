61f98657d5fdf0a19aacd75a4efceb9ea9798f90
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-10 17:34:35 +0100

    Rename races test file to fit convention.

--- api/tests/test_races_endpoint.py
+++ api/tests/test_races_endpoint.py
@@ -8,6 +8,7 @@ from flask_api import status
 
 from endpoints.races.races import races_endpoint
 
+
 class MockRace:
     def __init__(self, id, date, status, id_round):
         self.id = id
@@ -15,19 +16,19 @@ class MockRace:
         self.status = status
         self.id_round = id_round
 
+
 class MockRaceParticipants:
     def __init__(self, id, id_snail, id_race):
         self.id = id
         self.id_snail = id_snail
-        self.id_race = id_race
 
-class TestRaceEndpoint(TestCase):
+class TestRacesEndpoint(TestCase):
 
     def setUp(self):
         self.client = app.test_client()
 
     @mock.patch('db.models.Race.get_all_races', MagicMock(return_value=[MockRace(1, "2018-10-10 10:00:00", "RAINED OFF", 1)]))
-    @mock.patch('db.models.RaceParticipants.get_race_participants_race_id', MagicMock(return_value=[MockRaceParticipants(1,2,1), MockRaceParticipants(2,1,1)]))
+    @mock.patch('db.models.RaceParticipants.get_race_participants_race_id', MagicMock(return_value=[MockRaceParticipants(1, 2, 1), MockRaceParticipants(2, 1, 1)]))
     def test_races_authorized_body(self):
 
         with self.client as client:
@@ -42,12 +43,12 @@ class TestRaceEndpoint(TestCase):
             "status": "RAINED OFF",
             "id_round": 1,
             "id_snails": [2, 1]
-            }
-            ]
+        }
+        ]
         self.assertEqual(result, expected_result)
 
     @mock.patch('db.models.Race.get_all_races', MagicMock(return_value=[MockRace(1, "2018-10-10 10:00:00", "RAINED OFF", 1)]))
-    @mock.patch('db.models.RaceParticipants.get_race_participants_race_id', MagicMock(return_value=[MockRaceParticipants(1,2,1), MockRaceParticipants(2,1,1)]))
+    @mock.patch('db.models.RaceParticipants.get_race_participants_race_id', MagicMock(return_value=[MockRaceParticipants(1, 2, 1), MockRaceParticipants(2, 1, 1)]))
     def test_races_authorized_status_code(self):
         with self.client as client:
             response = client.get("/auth/token")
@@ -76,6 +77,7 @@ class TestRaceEndpoint(TestCase):
         with self.client as client:
             response = client.get('/races')
             self.assertTrue(status.is_client_error(response.status_code))
+
 
 if __name__ == '__main__':
     unittest.main()
