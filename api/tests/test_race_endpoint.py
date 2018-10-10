1fcf424f98816815f17f111f94d35e4e7a447d71
Sandeep Sharda <sandeep.sharda@infinityworks.com>
2018-10-10 14:57:28 +0100

    added initial tests for race endpoint

--- api/tests/test_race_endpoint.py
+++ api/tests/test_race_endpoint.py
@@ -1,40 +1,49 @@
 import unittest
-import sys
-sys.path.insert(0, '/vagrant/repos/snailx_api/api')
+from unittest import TestCase
+from unittest import mock
+from unittest.mock import MagicMock
+
 from globals.globals import app
+from flask_api import status
 
+from endpoints.races.races import races_endpoint
 
-class TestRaceEndpoint(unittest.TestCase):
+class MockRace:
+    def __init__(self, id, date, status, id_round):
+        self.id = id
+        self.date = date
+        self.status = status
+        self.id_round = id_round
+
+class MockRaceParticipants:
+    def __init__(self, id, id_snail, id_race):
+        self.id = id
+        self.id_snail = id_snail
+        self.id_race = id_race
+
+class TestRaceEndpoint(TestCase):
 
     def setUp(self):
-        self.app = app.test_client()
+        self.client = app.test_client()
 
-    def test_get_race_without_auth(self):
-        with self.app as client:
-            response = client.get("/race").get_json()
-            expected_result = {
-                'status': 'Failed',
-                'message': 'Unauthorized'
-            }
-            self.assertEqual(response, expected_result)
+    @mock.patch('db.models.Race.get_all_races', MagicMock(return_value=[MockRace(1, "2018-10-10 10:00:00", "RAINED OFF", 1)]))
+    @mock.patch('db.models.RaceParticipants.get_race_participants_race_id', MagicMock(return_value=[MockRaceParticipants(1,1,2), MockRaceParticipants(2,2,2)]))
+    def test_races_authorized_body(self):
 
-    def test_get_race_with_auth(self):
-        token = ""
-        with self.app as client:
-            response = client.get("/auth/token").get_json()
-            token = response['token']
+        with self.client as client:
+            response = client.get("/auth/token")
+            token = response.get_json()['token']
+            headers = {'Authorization': token}
 
-        headers = {'Authorization': token}
-        with self.app as client:
-            response = client.get("/race", headers=headers).get_json()
-            expected_result = {
+        result = client.get('/races', headers=headers).get_json()
+        expected_result = [{
             "id": 1,
-            "date": "Tue, 05 Sep 2017 09:45:28 GMT",
-            "status": "OK",
+            "date": "2018-10-10 10:00:00",
+            "status": "RAINED OFF",
             "id_round": 1,
             "id_snails": [1,2]
-            }
-            self.assertEqual(response, expected_result)
+            }]
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':
