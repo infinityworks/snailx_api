ad643675d38d14216964bf12b94cecca86049414
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-11 12:49:11 +0100

    Fix db query for round races.

--- api/db/models.py
+++ api/db/models.py
@@ -59,7 +59,7 @@ class Race(db.Model):
         return self.query.all()
 
     def get_round_race_ids(self, id_round):
-        return self.query(Race.id).filter_by(id_round=id_round).all()
+        return db.session.query(Race.id).filter_by(id_round=id_round).all()
 
 
 class Round(db.Model):
