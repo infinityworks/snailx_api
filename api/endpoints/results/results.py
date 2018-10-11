cc5e79c979060245df9b553e4e560fc31638d648
Michael Silverstone <michael.silverstone@infinityworks.com>
2018-10-11 16:35:24 +0100

    mikes random stuff wip

--- api/endpoints/results/results.py
+++ api/endpoints/results/results.py
@@ -1,4 +1,3 @@
-from flask_api import status
 from auth.auth import Auth
 from db.models import RaceResult, RaceParticipants
 from flask import Blueprint
@@ -25,17 +24,22 @@ def results_json(results_query_response):
 
     for results_query in results_query_response:
 
-        print(results_query.id_race_participants)
-        race_participants_from_id = race_participants_from_db(results_query.id_race_participants)
+        race_participants = RaceParticipants()
+        response_race_participants = race_participants.get_race_participants_race_id(results_query.id_race_participants)
 
-        json = {
-            "id_snail": race_participants_from_id.id_snail,
-            "position_snail": results_query.position,
-            "time_snail": results_query.time_to_finish,
-            "DNF": results_query.did_not_finish
-        }
+        json = []
+        for participants in response_race_participants:
+            race_results = RaceResult()
+            response_race_results = race_results.get_race_result(participants.id)
 
-        base_json.append({"id_race": race_participants_from_id.id_race, "snails": json})
+            json.append({
+                "id_snail": participants.id_snail,
+                "position_snail": response_race_results.position,
+                "time_snail": response_race_results.time_to_finish,
+                "DNF": response_race_results.did_not_finish
+            })
+
+        base_json.append({"id_race": participants.id_race, "snails": json})
 
     return base_json
 
@@ -46,17 +50,12 @@ def results_from_db():
 
     return results_query_response
 
-def race_participants_from_db(id):
-    race_participants_from_id = RaceParticipants().get_race_participants(id)
-    print(race_participants_from_id)
 
-    return race_participants_from_id
-
-    # return {
-    #     "id_race": 1,
-    #     "snails": [
-    #         {"id_snail": 1, "position_snail:": 3, "time_snail": 600, "DNF": False},
-    #         {"id_snail": 2, "position_snail:": 2, "time_snail": 500, "DNF": False},
-    #         {"id_snail": 3, "position_snail:": 1, "time_snail": 400, "DNF": False}
-    #     ]
-    # }
+# return {
+#     "id_race": 1,
+#     "snails": [
+#         {"id_snail": 1, "position_snail:": 3, "time_snail": 600, "DNF": False},
+#         {"id_snail": 2, "position_snail:": 2, "time_snail": 500, "DNF": False},
+#         {"id_snail": 3, "position_snail:": 1, "time_snail": 400, "DNF": False}
+#     ]
+# }
