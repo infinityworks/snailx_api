05185209b34a3473b40f0ba00d97d264e18a73ae
Junaid Zafar <junaid.zafar@infinityworks.com>
2018-10-11 16:20:38 +0100

    updated code

--- api/endpoints/results/results.py
+++ api/endpoints/results/results.py
@@ -1,13 +1,13 @@
 from flask_api import status
 from auth.auth import Auth
-from db.models import RaceResult
+from db.models import RaceResult, RaceParticipants
 from flask import Blueprint
 from globals.globals import app
 
 results_endpoint_blueprint = Blueprint('results', __name__)
 
 
-@results_endpoint_blueprint.route('/races/results')
+@results_endpoint_blueprint.route('/results/')
 def results_all():
     """GET end point to return results"""
 
@@ -18,40 +18,39 @@ def results_all():
     return results_json(results_from_db())
 
 
-def results_json(query_response):
+def results_json(results_query_response):
 
-    print("QUERY!! " + str(query_response))
 
-    array = []
+    base_json = []
 
-    for query in query_response:
+    for results_query in results_query_response:
 
-        if query.did_not_finish == 'False':
-            dnf = False
-        if query.did_not_finish == 'True':
-            dnf = True
-        if query.did_not_finish == '':
-            dnf = False
+        print(results_query.id_race_participants)
+        race_participants_from_id = race_participants_from_db(results_query.id_race_participants)
 
         json = {
-            "id_snail": query.id,
-            "position_snail": query.position,
-            "time_snail": query.time_to_finish,
-            "DNF": dnf
+            "id_snail": race_participants_from_id.id_snail,
+            "position_snail": results_query.position,
+            "time_snail": results_query.time_to_finish,
+            "DNF": results_query.did_not_finish
         }
 
-        array.append(json)
+        base_json.append({"id_race": race_participants_from_id.id_race, "snails": json})
 
-    return array
+    return base_json
 
 
 def results_from_db():
     result = RaceResult()
-    query_response = result.get_all_race_results()
+    results_query_response = result.get_all_race_results()
 
-    return query_response
+    return results_query_response
 
+def race_participants_from_db(id):
+    race_participants_from_id = RaceParticipants().get_race_participants(id)
+    print(race_participants_from_id)
 
+    return race_participants_from_id
 
     # return {
     #     "id_race": 1,
