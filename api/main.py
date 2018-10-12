97eab379ff36448096c821d12b4621c24182f825
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-12 14:59:52 +0100

    Remove unnecessary endpoint from main.

--- api/main.py
+++ api/main.py
@@ -3,20 +3,6 @@ sys.path.insert(0, '/vagrant/repos/snailx_api/api')
 from globals.globals import app
 import os
 
-
-@app.route('/races/results')
-def results():
-    """GET end point to return results"""
-    return {
-        "id_race": 1,
-        "snails": [
-            {"id_snail": 1, "position_snail:": 3, "time_snail": 600, "DNF": False},
-            {"id_snail": 2, "position_snail:": 2, "time_snail": 500, "DNF": False},
-            {"id_snail": 3, "position_snail:": 1, "time_snail": 400, "DNF": False},
-        ]
-    }
-
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
