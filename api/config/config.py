09429a96ac041df557de2cad0b0f3101411834ee
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-15 21:28:48 +0100

    Add env var config for postgres db.

--- api/config/config.py
+++ api/config/config.py
@@ -3,8 +3,24 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+def get_env_variable(name):
+    try:
+        return os.environ[name]
+    except KeyError:
+        message = "Expected environment variable '{}' not set.".format(name)
+        raise Exception(message)
+
+
+POSTGRES_URL = get_env_variable("POSTGRES_URL")
+POSTGRES_USER = get_env_variable("POSTGRES_USER")
+POSTGRES_PW = get_env_variable("POSTGRES_PW")
+POSTGRES_DB = get_env_variable("POSTGRES_DB")
+DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(
+    user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB)
+
+
 class Config:
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = DB_URL
