3de7c113ebc1675919ebc391ecc2a9062581b59b
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-16 09:57:52 +0100

    Refactor code in config.py

--- api/config/config.py
+++ api/config/config.py
@@ -3,24 +3,16 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-def get_env_variable(name):
-    try:
-        return os.environ[name]
-    except KeyError:
-        message = "Expected environment variable '{}' not set.".format(name)
-        raise Exception(message)
-
-
-POSTGRES_URL = get_env_variable("POSTGRES_URL")
-POSTGRES_USER = get_env_variable("POSTGRES_USER")
-POSTGRES_PW = get_env_variable("POSTGRES_PW")
-POSTGRES_DB = get_env_variable("POSTGRES_DB")
-DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(
-    user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB)
-
-
 class Config:
     DEBUG = False
     TESTING = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
+
+    POSTGRES_URL = os.getenv("POSTGRES_URL")
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PW = os.getenv("POSTGRES_PW")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(
+        user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = DB_URL
+
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
