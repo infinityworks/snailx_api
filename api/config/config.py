bbc0fa00951aea8a4bfd3a339aed3ac73aafeb26
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-16 14:23:46 +0100

    Add url for production database.

--- api/config/config.py
+++ api/config/config.py
@@ -12,7 +12,7 @@ class Config:
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgres://fqtkemerxaamnr:a50ea567ed6f24f9bfba2bd6071729d195df57a82c3bcc0ffcf07a176916d2d7@ec2-54-217-249-103.eu-west-1.compute.amazonaws.com:5432/d83g723rkf2r2o'
+    SQLALCHEMY_DATABASE_URI = 'postgres://wifosdkondwers:c4d03ddd8e3c2d404d828c1fbd6fea57fbc86700efc716ba7fd566a023044083@ec2-46-137-75-170.eu-west-1.compute.amazonaws.com:5432/d5ajcc7bafdnq3'
 
 
 class StagingConfig(Config):
