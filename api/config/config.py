93619406b2b819f5fe6822a6d2b1813ed5080150
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-16 13:53:04 +0100

    Update config.

--- api/config/config.py
+++ api/config/config.py
@@ -7,14 +7,6 @@ class Config:
     DEBUG = False
     TESTING = False
 
-    POSTGRES_URL = os.getenv("POSTGRES_URL")
-    POSTGRES_USER = os.getenv("POSTGRES_USER")
-    POSTGRES_PW = os.getenv("POSTGRES_PW")
-    POSTGRES_DB = os.getenv("POSTGRES_DB")
-    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(
-        user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB)
-    SQLALCHEMY_DATABASE_URI = DB_URL
-
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
 
 
@@ -30,5 +22,14 @@ class StagingConfig(Config):
 
 
 class DevelopmentConfig(Config):
+
+    POSTGRES_URL = os.getenv("POSTGRES_URL")
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PW = os.getenv("POSTGRES_PW")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(
+        user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = DB_URL
+
     DEVELOPMENT = True
     DEBUG = True
