9363e2ad42ae5bb728258a9ed10448459afa0cf7
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-17 14:13:02 +0100

    Add bcrypt password hashing to new user creation.

--- api/manage.py
+++ api/manage.py
@@ -2,6 +2,8 @@ import os
 from flask_script import Manager, prompt
 from flask_migrate import Migrate, MigrateCommand
 
+from flask_bcrypt import Bcrypt
+
 from globals.globals import app, db
 from config.config import Config
 from db.models import User
@@ -12,6 +14,7 @@ app.config.from_object(Config)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
+bcrypt = Bcrypt(app)
 
 
 @manager.command
@@ -27,7 +30,10 @@ def create_user():
         'Please enter an email address (max length 120 chars)')
     password = prompt(
         'Please enter a password (max length 128 chars)')
-    new_user = User(username=username, email=email, password=password)
+
+    hash_pw = bcrypt.generate_password_hash(password)
+
+    new_user = User(username=username, email=email, password=hash_pw)
     db.session.add(new_user)
     db.session.commit()
 
