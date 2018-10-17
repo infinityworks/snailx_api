c60959433aadfa1f95ecb3ee565a912ebf28834c
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-17 16:04:35 +0100

    Remove user code from auth endpoint.

--- api/endpoints/auth/auth.py
+++ api/endpoints/auth/auth.py
@@ -2,22 +2,8 @@ from flask import Blueprint, make_response
 from flask_api import status
 from auth.auth import Auth
 from globals.globals import app
-from db.models import User
 
 auth_endpoint_blueprint = Blueprint('auth', __name__)
-
-
-@auth_endpoint_blueprint.route('/auth/users')
-def get_user():
-    users = User().get_users()
-    if users:
-        users_json = []
-        for user in users:
-            users_json.append({'username': user.username,
-                               'email': user.email,
-                               'password': user.password})
-        json = {'users': users_json}
-    return make_response(json), status.HTTP_200_OK
 
 
 @auth_endpoint_blueprint.route('/auth/token')
