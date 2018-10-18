3704458a8cd7efe80e0c805aef4ce18daf73e404
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-18 14:07:11 +0100

    Fix init db command.

--- api/manage.py
+++ api/manage.py
@@ -1,5 +1,5 @@
 from flask_script import Manager
-from flask_migrate import MigrateCommand
+from flask_migrate import MigrateCommand, Migrate
 
 from globals.globals import app, db
 from config.config import Config
@@ -10,6 +10,7 @@ from commands.create_user_command import CreateUserCommand
 app.config.from_object(Config)
 
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
 manager.add_command('create_user', CreateUserCommand)
